17b11e4ed3cceb869ebb7bd816e1c84356d8176d
jiwon.kim <jiwon.kim@vivity.ai>
2024-12-30 15:08:19 +0900

    add annotation and update test

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-from cython_bbox import bbox_overlaps
+from cython_bbox import bbox_overlaps, bbox_ious
 import numpy as np
 
 gt = np.random.random((5, 4))
 dt = np.random.random((10, 4))
 
+# intersection over A
 overlaps = bbox_overlaps(
         np.ascontiguousarray(dt, dtype=np.float32),
         np.ascontiguousarray(gt, dtype=np.float32)
     )
 
+# intersection over union
+ious = bbox_ious(
+        np.ascontiguousarray(dt, dtype=np.float32),
+        np.ascontiguousarray(gt, dtype=np.float32)
+    )
+
 print(overlaps)
+print(ious)
